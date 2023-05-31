e13f20b3a638184fed36931ff2baa92281f6c22f
Samoxiaki <Samoxiaki@yahoo.com>
2023-05-31 12:59:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 import setuptools
+from pathlib import Path
 
 setuptools.setup(
 	name = "junkpy",
-	version = "0.0.2",
+	version = "0.0.3",
 	author = "Samoxiaki",
 	author_email = "samoxiaki@yahoo.com",
 	description = "Extended JSON Library for Configuration Files",
+	long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type='text/markdown',
 	packages = ["junkpy"],
 	install_requires = ["lark"]
 )
